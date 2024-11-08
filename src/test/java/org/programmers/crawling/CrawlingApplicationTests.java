ad2686007c6ae02362f5a7a2272db1f9f039779c
dongJ <alalal2234@gmail.com>
2024-11-08 17:59:20 +0900

    Initial commit

---
+++ src/test/java/org/programmers/crawling/CrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
