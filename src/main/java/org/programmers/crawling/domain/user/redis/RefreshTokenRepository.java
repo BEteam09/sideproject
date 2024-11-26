86f2dcf439850943c14fe461b4cb1b9f835c0f0c
jjongwon7 <jjongwon7@naver.com>
2024-11-26 17:23:18 +0900

    feat: Redis 환경 구성

---
+++ src/main/java/org/programmers/crawling/domain/user/redis/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package org.programmers.crawling.domain.user.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+}
